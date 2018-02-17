35a7388fb25150cff5f82bf264a36cc9ffcb508c
dnguyenv <dnguyenv@us.ibm.com>
2018-02-17 17:52:04 -0500

    update

--- see.js
+++ see.js
@@ -74,6 +74,7 @@ function parseText(text) {
     containsKno = (text.indexOf("know") >= 0),
     containsSee = (text.indexOf("see") >= 0);
   if ((containsDo || containsCan || containsYou) && (containsKno || containsSee || containsRec)) {
+    micInstance.stop();
     parseCommand(text);
     //console.log('test');
   }
@@ -112,6 +113,7 @@ var processSeeCommand = function(command) {
         .then(function(objects) {
           var str = objects[0].class;
           speak('OK, this looks like a ' + str);
+          micInstance.start();
         })
     })
   }, 3000);
